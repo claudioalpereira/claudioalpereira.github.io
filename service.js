6783568700e00ffa7d43e6a8f9a972e75d4aa468
Claudio Pereira <claudio_leal@hotmail.com>
2020-07-22 02:01:51 +0100

    added notification on push

--- service.js
+++ service.js
@@ -44,7 +44,8 @@ self.addEventListener("install", async () => {
 self.addEventListener("push", function(event) {
   if (event.data) {
     console.log("Push event!! ", event.data.text());
-  } else {
+	showLocalNotification("Yolo", event.data.text(),  self.registration);
+} else {
     console.log("Push event but no data");
   }
 });
